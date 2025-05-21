e9761d53776fa88627c67bbede72abdd0126395c
HrushiBorhade <hrushiborhade123@gmail.com>
2025-05-21 16:21:56 +0530

    unstable: add novel editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import SimpleEditor from "@/components/tiptap/simple";
+import TailwindAdvancedEditor from "@/components/editor"
 
 export default function Home() {
   return (
     <div className="w-full h-[100dvh]">
       <div className="container p-4 w-full mx-auto h-full flex items-center flex-col gap-4">
         <h1 className="text-4xl font-medium font-mono">Exploring Tiptap Editor</h1>
-        <SimpleEditor />
+        <TailwindAdvancedEditor />
       </div>
     </div>
   )
